61bc922aab7d62c516d0b29cce79c9d091b4bfab
arjunrajkumar-ds <arjun.rajkumar.98@gmail.com>
2022-05-17 14:54:39 +1000

    ch1 and ch2
    simple stuff

---
+++ chapter_2/names.py
@@ -0,0 +1,11 @@
+name = 'arjun rajkumar'
+
+# .title() function capitalises the first letter of each space-seperated string
+print(name.title())
+
+# f-strings:
+first_name = 'arjun'
+last_name = 'rajkumar'
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
