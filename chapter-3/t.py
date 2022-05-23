bb1935ede66c1ff3ce29b2a0c56ba03b4fa117e3
arjunrajkumar-ds <arjun.rajkumar.98@gmail.com>
2022-05-23 17:48:36 +1000

    ch-3 start

---
+++ chapter-3/t.py
@@ -0,0 +1,7 @@
+# 3-1 : 3-3 - Make a list of names and messages. Print personalised messages for each name
+
+names = ['vivian', 'nick', 'eddy']
+messages = ['ur tiring', 'ur massive', 'nice milf jacket']
+
+for i in range(len(names)):
+    print(f"{messages[i]},{names[i]}")
